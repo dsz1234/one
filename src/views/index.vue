3d5a8712982ad891eeea26802154b5f98f235765
dsz1234 <2634164883@qq.com>
2023-08-24 11:55:45 +0800

    feat(global): 结构调整

---
+++ src/views/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="container">
+    <div class="row"></div>
+  </div>
+</template>
+
+<script lang="ts" setup></script>
+
+<style lang="scss" scoped>
+.container {
+  width: 1000px;
+  height: 500px;
+  background-color: yellow;
+  .row {
+    width: 200px;
+    height: 200px;
+    background-color: red;
+  }
+}
+</style>
