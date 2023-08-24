c115c5507f9f4e8006097cb84ad7e136c5e534d8
dsz1234 <2634164883@qq.com>
2023-08-24 08:20:27 +0800

    fix(mtmy): change

--- src/main.ts
+++ src/main.ts
@@ -2,13 +2,15 @@ import './assets/main.css'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
-
+import piniaPersistedstate from 'pinia-plugin-persistedstate'
+const pinia = createPinia()
+pinia.use(piniaPersistedstate)
 import App from './App.vue'
 import router from './router'
 
 const app = createApp(App)
 
-app.use(createPinia())
+app.use(pinia)
 app.use(router)
 
 app.mount('#app')
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,19 @@
 <script setup lang="ts">
+import { useCounterStore } from '@/stores/counter'
+const store = useCounterStore()
+
+const add = () => {
+  store.increment()
+}
 import TheWelcome from '../components/TheWelcome.vue'
 </script>
 
 <template>
   <main>
+    {{ store.count }}
+    <button @click="add">++</button>
+
+    <h1>{{ store.doubleCount }}</h1>
     <TheWelcome />
   </main>
 </template>
